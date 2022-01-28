c59376955cae47580f3e861d51695d3c80e7d330
bournejj <82320254+bournejj@users.noreply.github.com>
2022-01-28 11:25:03 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
+const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require('./helpers');
+
+app.get('/mean', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "mean",
+    result: findMean(nums)
+  }
+
+  return res.send(result);
+});
+app.get('/median', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums)
+  }
+
+  return res.send(result);
+});
+app.get('/mode', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+
+  let result = {
+    operation: "median",
+    result: findMode(nums)
+  }
+
+  return res.send(result);
+});
+
+
+app.listen(3000, () => {
+    console.log('server is online')
+
+
+})
+
+
+  
